742fa1e93400c12ad9708ac4a71754e3761fe80e
greengram24 <pascuavhince24@gmail.com>
2026-05-22 01:23:13 +0800

    fixing page on mobile view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,17 +10,19 @@ import LoginPage from "./pages/LoginPage"
 
 const App = () => {
   return (
-    <div className="relative h-full w-full">
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]"/>
+    <div className="relative min-h-dvh w-full">
+      
+      {/* background */}
+      <div className="fixed inset-0 -z-10 bg-[radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
+
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/menu" element={<MenuPage/>} />
-        <Route path="/about" element={<AboutPage/>} />
-        {/* Gallery removed */}
-        <Route path="/reviews" element={<ReviewsPage/>} />
-        <Route path="/contact" element={<ContactPage/>} />
-        <Route path="/admin" element={<AdminPage/>} />
-        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/menu" element={<MenuPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/reviews" element={<ReviewsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
   )
